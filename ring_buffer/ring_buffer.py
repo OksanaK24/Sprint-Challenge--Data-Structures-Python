6d831c89a68f606fa89b48015ad428a7075cecbd
Oksana Kril <oksanakril@Oksanas-iMac.local>
2020-06-28 18:39:34 -0500

    ring_buffer passes all tests

--- ring_buffer/ring_buffer.py
+++ ring_buffer/ring_buffer.py
@@ -1,9 +1,21 @@
 class RingBuffer:
     def __init__(self, capacity):
-        pass
+        self.capacity = capacity
+        self.list = []
+        self.current_item_to_remove = 0
 
     def append(self, item):
-        pass
+        # if list length less than capacity
+        if len(self.list) < self.capacity:
+            self.list.append(item)
+        # if list length bigger than capacity
+        elif len(self.list) == self.capacity:
+            self.list.pop(self.current_item_to_remove)
+            self.list.insert(self.current_item_to_remove, item)
+            if self.current_item_to_remove < 4:
+                self.current_item_to_remove += 1
+            else:
+                self.current_item_to_remove = 0
 
     def get(self):
-        pass
+        return self.list
