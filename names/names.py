db08e50c36961141f563513907807b9f05019e99
Oksana Kril <oksanakril@Oksanas-iMac.local>
2020-06-28 20:19:05 -0500

    names.py - MVP done

--- names/names.py
+++ names/names.py
@@ -12,13 +12,64 @@ f.close()
 
 duplicates = []  # Return the list of duplicates in this data structure
 
-# Replace the nested for loops below with your improvements
-for name_1 in names_1:
-    for name_2 in names_2:
-        if name_1 == name_2:
-            duplicates.append(name_1)
+# # Replace the nested for loops below with your improvements (around runtime: 5.39583683013916 seconds)
+# for name_1 in names_1:
+#     for name_2 in names_2:
+#         if name_1 == name_2:
+#             duplicates.append(name_1)
 
-# The fastest way (Stretch Goal): (runtime: 0.0076220035552978516 seconds)
+
+# ----------------  Solution wiht Binary Search Tree  --------------------------
+# (runtime: 0.08461308479309082 seconds)
+class BSTNode:
+    def __init__(self, value):
+        self.value = value
+        self.left = None
+        self.right = None
+
+    # Insert the given value into the tree
+    def insert(self, value):
+        if value < self.value:
+            if self.left is None:
+                self.left = BSTNode(value)
+            else:
+                self.left.insert(value)
+        elif value >= self.value:
+            if self.right is None:
+                self.right = BSTNode(value)
+            else:
+                self.right.insert(value)
+
+    # Return True if the tree contains the value
+    # False if it does not
+    def contains(self, target):
+        if self.value == target:
+            return True
+        elif self.value > target:
+            if self.left is None:
+                return False
+            else:
+                return self.left.contains(target)
+        else:
+            if self.right is None:
+                return False
+            else:
+                return self.right.contains(target)
+
+# creating a BST for the first list of name
+all_names = None
+for name in names_1:
+    if all_names is None:
+        all_names = BSTNode(name)
+    else:
+        all_names.insert(name)
+
+# looping through the second list and use contains function to check for dublicates
+for name in names_2:
+    if all_names.contains(name):
+        duplicates.append(name)
+
+# -------------  The fastest way (Stretch Goal): (runtime: 0.0034160614013671875 seconds)  ------
 # duplicates = list(set(names_1).intersection(set(names_2)))
 
 end_time = time.time()
